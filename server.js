916254dee76d65bafb7246163f461bed1a718aaf
Selim Halac <halacselim@gmail.com>
2016-05-21 11:00:33 -0500

    changed port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
-const PORT=3000; 
+var PORT= process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('<h1>Hello World<h1>');
 }).listen(PORT, function(){
-	console.log('app listening on 3000');
+	console.log('app listening on 3000 or on env');
 });
