c049179b97caeedde23e91dd227b5ee5af16e886
Selim Halac <halacselim@gmail.com>
2016-05-21 10:40:39 -0500

    got it to work

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
-
-    var server = http.createServer();
-    server.listen(8001);
-
-
-    
+const PORT=3000; 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('<h1>Hello World<h1>');
+}).listen(PORT, function(){
+	console.log('app listening on 3000');
+});
